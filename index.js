6ed32e4f2fe527847c2cbaea941a2e43412e0469
root <twhtanghk@gmail.com>
2019-03-17 05:01:05 +0000

    replace coffee with js module

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-require('coffeescript/register');
-module.exports = require('./index.coffee');
+// please see https://andywalpole.me/blog/140739/using-javascript-create-guid-from-users-browser-information
+module.exports = function() {
+  var guid, nav, screen;
+  nav = window.navigator;
+  screen = window.screen;
+  guid = nav.mimeTypes.length;
+  guid += nav.userAgent.replace(/\D+/g, '');
+  guid += nav.plugins.length;
+  guid += screen.height || '';
+  guid += screen.width || '';
+  guid += screen.pixelDepth || '';
+  return guid;
+};
